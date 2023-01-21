4d50abea5a8f7c27f89ecc7849fa09279c8b1a84
Jeyte Hagaley <jahagaley@hotmail.com>
2023-01-21 12:56:38 -0800

    Adding Postgres model and setup (#2)
    * Adding Postgres model and setup
    
    * small changes
    
    * clean up

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-from pb.example.v1.services.store_pb2 import CreateStoreRequest
-from pb.example.v1.services.store_pb2 import CreateStoreResponse
-
-from pb.example.v1.services import store_pb2_grpc
-from server.services.store import StoreService
-
-import grpc
-from concurrent import futures
-
 import logging
+from concurrent import futures
+import grpc
+
+from pb.postseam.example.v1 import store_service_pb2_grpc
+
+from server.db.store_model import StoreModel
+from server.db.util import get_engine
+from server.services.store import StoreService
 
 logging.basicConfig(
     level=logging.INFO, format="[%(asctime)s] %(name)-12s [%(levelname)s] %(message)s"
 )
 
+
+def validate_tables():
+    engine = get_engine()
+
+    # Ensure all tables are created correctly
+    StoreModel.__table__.create(bind=engine, checkfirst=True)
+
+
 def serve():
+
+    validate_tables()
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=3))
 
-    store_pb2_grpc.add_StoreServiceServicer_to_server(StoreService(), server)
+    store_service_pb2_grpc.add_StoreServiceServicer_to_server(StoreService(), server)
 
-    server.add_insecure_port('0.0.0.0:8000')
+    server.add_insecure_port("0.0.0.0:8000")
     server.start()
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     serve()
--- test/server/services/test_store.py
+++ test/server/services/test_store.py
@@ -1,4 +1,5 @@
 import unittest
+import mock
 
 # Importing store model used requests.
 from pb.postseam.example.v1.store_pb2 import Store
@@ -10,44 +11,65 @@ from pb.postseam.example.v1.store_service_pb2 import CreateStoreRequest
 # gRPC store service implementation 
 from server.services.store import StoreService
 
+# Database imports
+from server.db.store_model import StoreModel
+
+
+def get_test_store_model():
+    return StoreModel(
+        id="test_id", email="email@email.com", business_name="Test Business"
+    )
+
 
 class TestStoreService(unittest.TestCase):
     def setUp(self):
         self.store_service = StoreService()
 
-    def test_CreateStore(self):
-        """ Expect to get CreateStoreResponse """
-        
-        request = CreateStoreRequest(
-            id="test_id",
-            email="test@test.com",
-            business_name="Test Business",
+    def get_mock_session(self, mock_session):
+        mock_session.return_value.__enter__.return_value.query.return_value.where.return_value.first.return_value = (
+            get_test_store_model()
         )
 
+    @mock.patch("server.services.store.get_engine")
+    def test_CreateStore(self, mock_engine):
+        """ Expect to get CreateStoreResponse """
+
+        # mocking some things
+        mock_store_model = get_test_store_model()
+
+        request = CreateStoreRequest(
+            id=mock_store_model.id,
+            email=mock_store_model.email,
+            business_name=mock_store_model.business_name,
+        )
         response = self.store_service.CreateStore(request, None)
 
         self.assertIsInstance(response, Store)
 
-        # Validating response proto fields 
+        # Validating response proto fields
         self.assertEqual(response.id, request.id)
         self.assertEqual(response.email, request.email)
         self.assertEqual(response.business_name, request.business_name)
 
-    def test_GetStore(self):
+    @mock.patch("server.services.store.get_engine")
+    @mock.patch("server.services.store.Session")
+    def test_GetStore(self, mock_session, mock_engine):
         """ Expect to get CreateStoreResponse """
-        
-        request = GetStoreRequest(
-            store_id="test_id",
-        )
 
+        # mocking some things
+        self.get_mock_session(mock_session)
+        mock_store_model = get_test_store_model()
+
+        request = GetStoreRequest(store_id=mock_store_model.id)
         response = self.store_service.GetStore(request, None)
 
         self.assertIsInstance(response, Store)
 
-        # Validating response proto fields 
-        self.assertEqual(response.id, request.store_id)
-        self.assertEqual(response.email, "email@email.com")
-        self.assertEqual(response.business_name, "Test Business")
+        # Validating response proto fields
+        self.assertEqual(response.id, mock_store_model.id)
+        self.assertEqual(response.email, mock_store_model.email)
+        self.assertEqual(response.business_name, mock_store_model.business_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
