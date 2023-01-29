eb2d72c29b8b395b007f54891da3371fde7cc15d
Jeyte Hagaley <jahagaley@hotmail.com>
2023-01-28 18:23:29 -0800

    Adding configs for Cloud SQL and Cloud Run (#4)

---
+++ server/util/settings.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+from dotenv import dotenv_values
+
+ENV_PATH = os.path.join(os.environ.get("ENV_FILE_PATH", "./"), ".env")
+config = dotenv_values(ENV_PATH)
+
+
+GCP_PROJECT = config.get("GCP_PROJECT", "")
+GCP_SQL_REGION = config.get("GCP_SQL_REGION", "")
+GCP_SQL_INSTANCE = config.get("GCP_SQL_INSTANCE", "")
+GCP_SQL_DATABASE = config.get("GCP_SQL_DATABASE", "")
+GCP_SQL_USER = config.get("GCP_SQL_USER", "")
+GCP_SQL_PASSWORD = config.get("GCP_SQL_PASSWORD", "")
